9b666de0bae518b2088f82f4f6973852bad7950a
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-13 19:09:09 +0000

    Deploying to gh-pages from @ Dalgoorak/goit-js-hw-09@8babaf46358b0d71fc9b7f0ea88b6efea57e9699 🚀

---
+++ 01-color-switcher.909d1fe7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body,a=null;function d(){n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){a=setInterval(d,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.909d1fe7.js.map
---
+++ 01-color-switcher.bfbf6c96.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body;let d=null;function a(){n.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){d=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.bfbf6c96.js.map
