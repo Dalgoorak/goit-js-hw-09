7427d5a98b16d35c1f766775fbb3d5538d4635ff
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-17 19:29:21 +0300

    add task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,92 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const text = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('button[data-start');
+const timerDiv = document.querySelector('.timer');
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
+
+let userDate = null;
+btnStart.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      btnStart.disabled = true;
+      userDate = new Date();
+    } else {
+      btnStart.disabled = false;
+      userDate = selectedDates[0];
+    }
+  },
+};
+
+flatpickr(text, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+const timer = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = userDate - currentTime;
+      const timeComponents = convertMs(deltaTime);
+
+      secondsEl.textContent = addLeadingZero(timeComponents.seconds);
+      minutesEl.textContent = addLeadingZero(timeComponents.minutes);
+      hoursEl.textContent = addLeadingZero(timeComponents.hours);
+      daysEl.textContent = addLeadingZero(timeComponents.days);
+      if (deltaTime <= 0) {
+        this.stop();
+        timerDiv.innerHTML = 'Time is over!';
+        Notiflix.Notify.success('Countdown finished');
+      }
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  },
+};
+
+btnStart.addEventListener('click', () => {
+  timer.start();
+});
