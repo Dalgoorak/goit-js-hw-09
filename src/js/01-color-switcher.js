41b0e808af484381d73d0f0bbc219bde8d5121ad
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-19 22:17:20 +0300

    correct mistakes in task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,8 +4,10 @@ const bodyRef = document.body;
 
 let timerId = null;
 const INTERVAL_DELAY = 1000;
-startBtn.disabled = false;
-stopBtn.disabled = true;
+
+onBtnDisabled(false, true);
+// startBtn.disabled = false;
+// stopBtn.disabled = true;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -14,18 +16,25 @@ function updateBackgroundColor() {
   bodyRef.style.background = getRandomHexColor();
 }
 
+function onBtnDisabled(start, stop) {
+  startBtn.disabled = start;
+  stopBtn.disabled = stop;
+}
+
 startBtn.addEventListener('click', onBtnClickStart);
 
 function onBtnClickStart() {
   timerId = setInterval(updateBackgroundColor, INTERVAL_DELAY);
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
+  // startBtn.disabled = true;
+  // stopBtn.disabled = false;
+  onBtnDisabled(true, false);
 }
 
 stopBtn.addEventListener('click', onBtnClickStop);
 
 function onBtnClickStop() {
   clearInterval(timerId);
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
+  // startBtn.disabled = false;
+  // stopBtn.disabled = true;
+  onBtnDisabled(false, true);
 }
